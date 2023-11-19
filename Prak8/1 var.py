021cedeab90fa70f8e5a762a349db6bdedef341f
smolb4 <145501808+smolb4@users.noreply.github.com>
2023-11-19 19:49:59 +0300

    Update 1 var.py

--- Prak8/1 var.py
+++ Prak8/1 var.py
@@ -23,3 +23,62 @@ print(k, s)
 
 #1. Вычислить сумму и число положительных элементов матрицы A[N, N],
 #находящихся над главной диагональю.
+
+print ('Задание 2')
+
+a=[]
+n=3
+m=4
+for i in range(n):
+    b=[]
+    for j in range(m):
+        b.append(int(input('Введите элементы матрицы: ')))
+    a.append(b)
+for i in a:
+    print(i)
+
+maximum=a[0][0]
+minimum=a[0][-1]
+for i in range(n):
+    for j in range(m):
+        if maximum<a[0][j]:
+
+            maximum=a[0][j]
+            a[0][0], a[0][j] = a[0][j], a[0][0]
+
+        if minimum>a[0][j]:
+            minimum=a[0][j]
+            a[0][-1], a[0][j] = a[0][j], a[0][-1]
+
+maximum=a[1][0]
+minimum=a[1][-1]
+for i in range(n):
+    for j in range(m):
+        if maximum<a[1][j]:
+            maximum=a[1][j]
+            a[1][0], a[1][j] = a[1][j], a[1][0]
+        if minimum>a[1][j]:
+            minimum=a[1][j]
+            a[1][-1], a[1][j] = a[1][j], a[1][-1]
+
+
+maximum=a[2][0]
+minimum=a[2][-1]
+for i in range(n):
+    for j in range(m):
+        if maximum<a[2][j]:
+            maximum=a[2][j]
+            a[2][0], a[2][j] = a[2][j], a[2][0]
+        if minimum>a[2][j]:
+            minimum=a[2][j]
+            a[2][-1], a[2][j] = a[2][j], a[2][-1]
+
+print('Измененный массив:')
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end=' ')
+    print()
+
+#2. Дана матрица B[N, М]. Найти в каждой строке матрицы
+#максимальный и минимальный элементы и поменять их с первым и
+#последним элементами строки соответственно.
