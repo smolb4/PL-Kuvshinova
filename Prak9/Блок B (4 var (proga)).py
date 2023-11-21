8dff64ecd52e5e1cae7a2235d187e2ff92ba60fa
smolb4 <145501808+smolb4@users.noreply.github.com>
2023-11-21 23:26:16 +0300

    Update Блок B (4 var (proga)).py

--- Prak9/Блок B (4 var (proga)).py
+++ Prak9/Блок B (4 var (proga)).py
@@ -2,20 +2,20 @@ print ('Задание 4')
 
 print ('Блок B')
 
-def pomogite(n, div=None):
-    if div is None:
-        div= n - 1
-    while div >= 2:
-        if n%div==0:
-            print('NO')
-            return n
+def pomogite(x, d=0):
+    if d==0:
+        d= x - 1
+    while d >= 2:
+        if x%d==0:
+            return print('NO')
         else:
-            return pomogite(n, div-1)
+             return pomogite(x, d-1)
     else:
-        print('YES')
-        return True
+        return print('YES')
+    pass
 n = int(input('Введите число (>1): '))
+pomogite(n)
 
 #4. Дано натуральное число n>1. Проверьте, является ли оно простым.
 #Программа должна вывести слово YES, если число простое и NO, если
-#число составное. 
+#число составное.
