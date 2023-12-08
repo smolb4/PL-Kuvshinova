fd681656aba5bb40273bb9f719897fa87310de80
smolb4 <145501808+smolb4@users.noreply.github.com>
2023-12-08 23:48:13 +0300

    Add files via upload

---
+++ jjson.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import Checkbutton
+####
+import requests
+import json
+from pprint import pprint
+
+window = Tk()
+window.title('Кувшинова Анастасия Алексеевна')
+window.geometry('400x100')
+
+label_rep = Label(window, text='Введите в поле имя репозитория: ')
+label_rep.pack()
+txt = Entry(window)
+txt.pack()
+
+def jjson():
+    username = txt.get()
+    url = f'https://api.github.com/users/{username}'
+    user_data = requests.get(url).json()
+    data = {}
+    kkey = ['company', 'created_at', 'email', 'id', 'name', 'url']
+    for i in kkey:
+        data[i] = user_data[i]
+    with open('Kuvshinova_Anastasia_Alekseevna_UB-31_jjson.json', 'w') as repoz:
+        json.dump(data, repoz)
+    print('Данные о репозитории получены и записаны в файл!')
+
+Button(window, text='Получить результат', command=jjson).pack()
+
+window.mainloop()
+
+#даны самые популярные репозитории на github
+#https://habr.com/ru/post/453444/, по последней цифре зачетки получить JSON   (2, spark)
+#для вашего варианта .
+#Программа с графическим интерфейсом вводим в поле имя репозитория и по
+#нажатию кнопки получаем результат.
+#Необходимо получить в новый файл следующую информацию:
+#   'company': None,
+#   'created_at': '2015-08-03T17:55:43Z',
+#   'email': None,
+#   'id': 13629408,
+#   'name': 'Kubernetes',
+#   'url': 'https://api.github.com/users/kubernetes'}
