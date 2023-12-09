0e5dceff5d9f4bfcf1c4af974f136ed178589d18
smolb4 <145501808+smolb4@users.noreply.github.com>
2023-12-09 18:06:03 +0300

    Add files via upload

---
+++ Prak6/7 вариант.py
@@ -0,0 +1,34 @@
+from random import randint
+
+print('Вариант 7')
+print('Задание 1')
+n = 10
+d = []
+for i in range(n):
+    d.append(randint(1, 10))
+print(d)
+summa = 0
+proiz = 1
+for i in range(len(d)):
+    if i%2==0:
+        summa += d[i]
+    else:
+        proiz *= d[i]
+print(summa, proiz)
+#1. Дан массив целых чисел. Найти сумму элементов с четными номерами и
+#произведение элементов с нечетными номерами. Вывести сумму и
+#произведение.
+
+print('Задание 2')
+m1 = 10
+n = []
+for i in range(m1):
+    n.append(randint(1, 10))
+print(n)
+a = n.index(max(n))
+b = n.index(min(n))
+
+n[a], n[b] = n[b], n[a]
+print(n)
+
+#2. Переставить в одномерном массиве минимальный элемент и максимальный
