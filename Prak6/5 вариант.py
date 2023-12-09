7f48f74928fe50d5f34c5ced6d2bbef94db517a4
smolb4 <145501808+smolb4@users.noreply.github.com>
2023-12-09 17:36:03 +0300

    Add files via upload

---
+++ Prak6/5 вариант.py
@@ -0,0 +1,30 @@
+from random import randint
+
+print('Вариант 4')
+print('Задание 1')
+n = 10
+d = []
+for i in range(n):
+    d.append(randint(-50, 50))
+print(d)
+for i in range(len(d)):
+    if d[i-1]<0 and d[i]<0:
+        print(d[i-1], d[i])
+
+#1. Дан одномерный массив из 10 целых чисел. Вывести пары отрицательных
+#чисел, стоящих рядом
+
+print('Задание 2')
+m1 = 10
+n = []
+for i in range(m1):
+    n.append(randint(10, 12))
+print(n)
+k = []
+for i in range(len(n)):
+    if n[i-1] != n[i]:
+        k.append(n[i])
+print(k)
+
+#2. Дан целочисленный массив размера 10. Создать новый массив, удалив все
+#одинаковые элементы, оставив их 1 раз.
